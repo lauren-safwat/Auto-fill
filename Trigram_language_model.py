2b179d1c1cfba2dde762c21fe73037886e852964
Lauren Safwat <36446574+lauren-safwat@users.noreply.github.com>
2021-05-10 13:40:24 +0200

    Simple modifications

--- Trigram_language_model.py
+++ Trigram_language_model.py
@@ -55,29 +55,34 @@ def calcProb(nGrams, nGramModel):
 
 def writeToFile(fileName, nGramModel):
     path = "E:\\FCAI\\4th year , 2nd semester\\NLP\\Assignments\\Assignment 1\\" + fileName
-    nGramModel.to_csv(path, mode='w', sep='\t', float_format='%.3f', encoding='UTF-8')
+    nGramModel.to_csv(path, mode='w', float_format='%.3f', encoding='UTF-8', header=True, index=True)
 
 # -----------------------------------------------------------------------------
 
-# Main function
-prepareCorpus()
+def trainModel():
+    prepareCorpus()
 
-nGrams(1, unigrams)
-nGrams(2, bigrams)
-nGrams(3, trigrams)
+    nGrams(1, unigrams)
+    nGrams(2, bigrams)
+    nGrams(3, trigrams)
 
-bigramModel = pd.DataFrame(index=set(unigrams), columns=set(unigrams), data=0)
-trigramModel = pd.DataFrame(index=set(bigrams), columns=set(unigrams), data=0)
+    bigramModel = pd.DataFrame(index=set(unigrams), columns=set(unigrams), data=0)
+    trigramModel = pd.DataFrame(index=set(bigrams), columns=set(unigrams), data=0)
 
-calcFreq(bigrams, bigramModel)
-calcFreq(trigrams, trigramModel)
+    calcFreq(bigrams, bigramModel)
+    calcFreq(trigrams, trigramModel)
 
-calcProb(unigrams, bigramModel)
-calcProb(bigrams, trigramModel)
+    calcProb(unigrams, bigramModel)
+    calcProb(bigrams, trigramModel)
 
-writeToFile("Bigram.csv", bigramModel)
-writeToFile("Trigram.csv", trigramModel)
+    writeToFile("Bigram.csv", bigramModel)
+    writeToFile("Trigram.csv", trigramModel)
 
+# -----------------------------------------------------------------------------
+
+# Main:
+
+trainModel()
 # print(tokens)
 # print()
 # print("\n".join(bigrams))
--- main.py
+++ main.py
@@ -1,1 +1,30 @@
 # Auto-completion of sentences Program using NLP with a simple GUI
+
+import pandas as pd
+
+#Program Global Variables
+
+# -----------------------------------------------------------------------------
+
+def readData():
+    path = "E:\\FCAI\\4th year , 2nd semester\\NLP\\Assignments\\Assignment 1\\Bigram.csv"
+    path2 = "E:\\FCAI\\4th year , 2nd semester\\NLP\\Assignments\\Assignment 1\\Trigram.csv"
+    bigram = pd.read_csv(path, encoding='UTF-8', header=0, index_col=0)
+    trigram = pd.read_csv(path2, encoding='UTF-8', header=0, index_col=0)
+    return bigram, trigram
+
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+
+# Main:
+
+bigramModel, trigramModel = readData()
+print(bigramModel)
+print()
+print(trigramModel)
+
+# -----------------------------------------------------------------------------
